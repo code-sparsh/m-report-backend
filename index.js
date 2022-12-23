b97bac2f9a57cb8f5e300a180b5c7f5ae4235f4f
Sparsh Sethi <sparshsethi15@gmail.com>
2022-12-24 04:51:37 +0530

    final changes

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+
+
+
+
 const routes = require('./routes/app');
 
 const app = express();
 const PORT = process.env.port || 4000;
 
-app.use(bodyParser.json());
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+
+app.use(bodyParser.json());
 // connect to mongodb
 mongoose.connect('mongodb://localhost/ninjago');
 mongoose.Promise = global.Promise;
 
-
-
+app.use(express.static('public'));
 
 app.use('/',routes);
 
--- routes/app.js
+++ routes/app.js
@@ -9,33 +9,38 @@ const fs = require('fs');
 
 const parserObj = new Parser();
 
+router.get('/upload',(req,res) => {
+
+    res.sendFile(~(__dirname) + 'public/upload.html');
+});
+
 
 router.get('/',(req,res) => {
 
-    res.sendFile("C:/Users/Spars/Projects/Health-backend/index.html");
+    res.sendFile(~(__dirname) + 'public/index.html');
 });
 
-// get user data from db
-router.get('/user', (req,res) => {
-    res.send({type: "GET"});
+router.get('/form',(req,res) => {
+
+    res.sendFile(~(__dirname) + 'public/form/index.html');
 });
 
-router.post('/heart', (req,res) => {
-    // Heart.create(req.body).then( (heart) => {
-    //     res.send(heart);
-    // })
 
+
+router.post('/form', (req,res) => {
+
+    // const json = JSON.stringify(req.body);
     const csv = parserObj.parse(req.body);
 
-    console.log(csv);
-    console.log(req.body.sex);
+    // console.log(csv);
 
+    console.log(req.body); 
 
     fs.writeFileSync('./test.csv',csv);
 
-    res.send("not found");
-    
-})
+    // res.sendFile(~(__dirname) + 'public/index.html');
+    res.send(req.body);
+});
 
 router.post('/user', (req,res) => {
     User.create(req.body).then((user) => {
